0039d6eebeef76b505a369ce5baffe59a138c91c
Erin Tustin <erintustin@gmail.com>
2022-11-01 17:01:09 -0400

    Build App File Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-    hello world
-</div>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
