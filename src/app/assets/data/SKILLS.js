7fdee17ecc46bba6056b2e8aae231a03d38b66b4
Erin Tustin <erintustin@gmail.com>
2022-11-04 11:33:22 -0400

    Add Skills data

--- src/app/assets/data/SKILLS.js
+++ src/app/assets/data/SKILLS.js
@@ -1,5 +1,6 @@
 import accessiblity from '../images/skills/accessiblity.png';
 import adobexd from '../images/skills/adobexd.png';
+import animated from '../images/skills/animated.png';
 import aws from '../images/skills/aws.png';
 import bootstrap from '../images/skills/bootstrap.png';
 import css from '../images/skills/css.png';
@@ -7,31 +8,52 @@ import expo from '../images/skills/expo.png';
 import express from '../images/skills/express.png';
 import firebase from '../images/skills/firebase.png';
 import git from '../images/skills/git.png';
+import github from '../images/skills/github.png';
 import googlecloud from '../images/skills/google-cloud.png';
 import html from '../images/skills/html.png';
 import javascript from '../images/skills/javascript.png';
+import jsx from '../images/skills/jsx.png';
 import mongo from '../images/skills/mongo.png';
+import navigation from '../images/skills/react-navigation.png';
 import node from '../images/skills/node.png';
 import photoshop from '../images/skills/photoshop.png';
+import postman from '../images/skills/postman.png';
 import reactnative from '../images/skills/react-native.png';
 import react from '../images/skills/react.png';
+import redux from '../images/skills/redux.svg';
+import router from '../images/skills/router.png';
+import sass from '../images/skills/sass.png';
+
+
 
 export const SKILLS = [
     {
-        name: 'HTML',
+        name: 'HTML5',
         image: html,
         type: 'language',
         featured: true
     },
     {
-        name: 'CSS',
+        name: 'CSS3',
         image: css,
+        type: 'language',
+        featured: true
+    },
+    {
+        name: 'SASS',
+        image: sass,
         type: 'language',
         featured: true
     },
     {
         name: 'JavaScript',
         image: javascript,
+        type: 'language',
+        featured: true
+    },
+    {
+        name: 'JSX',
+        image: jsx,
         type: 'language',
         featured: true
     },
@@ -44,12 +66,24 @@ export const SKILLS = [
     {
         name: 'Bootstrap',
         image: bootstrap,
-        type: 'framework',
+        type: 'library',
         featured: true
     },
     {
         name: 'React',
         image: react,
+        type: 'library',
+        featured: true
+    },
+    {
+        name: 'React Redux',
+        image: redux,
+        type: 'library',
+        featured: true
+    },
+    {
+        name: 'React Router',
+        image: router,
         type: 'framework',
         featured: true
     },
@@ -57,6 +91,18 @@ export const SKILLS = [
         name: 'React Native',
         image: reactnative,
         type: 'framework',
+        featured: true
+    },
+    {
+        name: 'React Navigation',
+        image: navigation,
+        type: 'api',
+        featured: true
+    },
+    {
+        name: 'React Native Animated',
+        image: animated,
+        type: 'api',
         featured: true
     },
     {
@@ -99,6 +145,18 @@ export const SKILLS = [
         name: 'Git',
         image: git,
         type: 'version control',
+        featured: true
+    },
+    {
+        name: 'GitHub',
+        image: github,
+        type: 'version control',
+        featured: true
+    },
+    {
+        name: 'Postman',
+        image: postman,
+        type: 'testing',
         featured: true
     },
     {
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,13 +5,11 @@ const Summary = () => {
     return(
         <Container className='summary mx-auto col-8'>
             <div className='h2 content'>Hi, I'm Erin!</div>
-            <img src={avatar} alt='self-portrait' className='avatar' />
+                <img src={avatar} alt='self-portrait' className='avatar' />
             <div className='content'>
-                I’m an energetic and adaptable recent bootcamp graduate seeking a junior developer role.
-                <br /> <br />
+                I’m a well-organized, energetically persistent, highly adaptable, and generally unflappable developer 
+                with prior experiences in the aviation and real estate industries. 
                 I specialize in mobile-first and responsive design with a focus on accessibility.
-                <br/> <br />
-                I have a passion for creating user-friendly, impactful web and mobile tools that can help make the world more inclusive and accessible to everyone. 
             </div>
                 <ButtonGroup className='main-buttons'>
                     <Button className='m-1'>Projects</Button>
